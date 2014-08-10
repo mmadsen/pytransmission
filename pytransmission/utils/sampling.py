59d0ce9710958e93282d5ecf332d1dd92869abba
Mark Madsen <mark@madsenlab.org>
2014-08-09 17:18:39 -0700

    don't keep empty counts in the sampling of dicts

--- pytransmission/utils/sampling.py
+++ pytransmission/utils/sampling.py
@@ -68,7 +68,7 @@ def get_sampled_dict_counts(ssize_list, dcounts):
 
         sampled_counts = npr.multinomial(ssize,prob,size=1)
         count_list = sampled_counts.tolist()
-        log.debug("traits: %s total: %s prob: %s counts: %s", traits, total, prob, count_list)
+        #log.debug("traits: %s total: %s prob: %s counts: %s", traits, total, prob, count_list)
         sampled_dict = dict(zip(traits, count_list[0]))
         result[ssize] = sampled_dict
 
