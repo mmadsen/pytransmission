053bbd8888b229feccc784d6322f9db2cdfeabcc
Mark Madsen <mark@madsenlab.org>
2015-02-22 16:20:30 -0800

    Initial allelic distribution function, porting from CTPy to remove that dependency for current and future projects.

--- pytransmission/popgen/__init__.py
+++ pytransmission/popgen/__init__.py
@@ -15,3 +15,20 @@ from wright_fisher import wfia_convergence_to_stationarity_generations, wf_mutat
 
 
 from crp import simulate_crp_memberships, get_crp_unlabeled_counts
+
+def constructUniformAllelicDistribution(numalleles):
+    """Constructs a uniform distribution of N alleles in the form of a frequency list.
+
+        Args:
+
+            numalleles (int):  Number of alleles present in the initial population.
+
+        Returns:
+
+            (list):  Array of floats, giving the initial frequency of N alleles.
+
+    """
+    divisor = 100.0 / numalleles
+    frac = divisor / 100.0
+    distribution = [frac] * numalleles
+    return distribution
